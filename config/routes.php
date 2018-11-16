bbc8dea1e017a5c80cb9659adbe16d1c3ac9d6c1
PaulaoDev <jskhanframework@gmail.com>
2018-11-16 20:36:37 -0200

    update router

--- config/routes.php
+++ config/routes.php
@@ -1,28 +1,30 @@
 <?php
 
+use \App\Khan\Component\Router\Router;
+
 Container::bind("teste", function () {
 	return "Comece o desenvolvimento!!";
 });
 
-Router::notFound(function ($req, $res) {
+Router\notFound(function ($req, $res) {
 	$res->setStatusCode(404);
 	return die("Route not found insert in router!!");
 });
 
-Router::get('/', function ($req, $res) {
+Router\get('/', function ($req, $res) {
 	$message = Container::get('teste')();
 	$res->render('index.html', [
 		'message' => $message,
 	]);
 });
 
-Router::get('/form', function ($req, $res) {
+Router\get('/form', function ($req, $res) {
 	return $res->render('csrf.html');
 });
 
-Router::post('/form', function ($req, $res) {
-	return Router::csrf_token_verify($req->post('token'))
+Router\post('/form', function ($req, $res) {
+	return Router\Router::csrf_token_verify($req->post('token'))
 	? 'verdadeiro' : 'falso';
 });
 
-Router::get('/teste', "Controllers\TesteController->index");
+Router\get('/teste', "Controllers\TesteController->index");
